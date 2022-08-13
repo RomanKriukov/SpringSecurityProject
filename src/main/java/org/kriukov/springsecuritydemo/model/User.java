9237d8a024568f1acf3f940f7b46e637b83b7008
Roman Kriukov <romank.official@gmail.com>
2022-08-13 23:57:01 +0300

    added authenticated with user in database

--- src/main/java/org/kriukov/springsecuritydemo/model/User.java
+++ src/main/java/org/kriukov/springsecuritydemo/model/User.java
@@ -1,10 +1,14 @@
 package org.kriukov.springsecuritydemo.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
--- src/main/java/org/kriukov/springsecuritydemo/security/UserDetailsServiceImpl.java
+++ src/main/java/org/kriukov/springsecuritydemo/security/UserDetailsServiceImpl.java
@@ -1,12 +1,27 @@
 package org.kriukov.springsecuritydemo.security;
 
+import org.kriukov.springsecuritydemo.model.User;
+import org.kriukov.springsecuritydemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service("userDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User with email " + email + " doesn't exists"));
+        return SecurityUser.fromUser(user);
     }
 }
